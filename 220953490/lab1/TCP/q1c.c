0b32d4c91ed20f3339d00aec0570be954b64079d
siddharth <144979580+Sid141004@users.noreply.github.com>
2025-01-27 16:10:49 +0530

    Create q1c.c

---
+++ 220953490/lab1/TCP/q1c.c
@@ -0,0 +1,130 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<sys/socket.h>
+#include<sys/types.h>
+#include<netinet/in.h>
+#include<sys/stat.h>
+#include<fcntl.h>
+#include<string.h>
+#include<stdlib.h>
+#define MAXSIZE 50
+
+main()
+{
+	char buff[MAXSIZE];
+	int sockfd,retval,i;
+	int recedbytes,sentbytes;
+	struct sockaddr_in serveraddr;
+	sockfd=socket(AF_INET,SOCK_STREAM,0);
+	if(sockfd==-1)
+	{
+		printf("\nSocket Creation Error");
+		return;
+	}
+	printf("CLIENT socket created (server port number -> 3388). \n");
+	serveraddr.sin_family=AF_INET;
+	serveraddr.sin_port=htons(3388);
+	serveraddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+	retval=connect(sockfd,(struct sockaddr*)&serveraddr,sizeof(serveraddr));
+	if(retval==-1)
+	{
+		printf("Connection error");
+		return;
+	}
+	printf("connected to server on port 3388. \n");
+
+	int flag = 1;
+
+	for (i = 0; ; i+=1)
+	{
+		int n, flag = 1;
+		printf("\nEnter the number of integers : ");
+		scanf("%d",&n);
+
+		int arr[n];
+		printf("\nEnter the integers\n");
+		for(int i  = 0 ; i < n ; i++){
+            scanf("%d",&arr[i]);
+		}
+
+		sentbytes=send(sockfd,&n,sizeof(n),0);
+		if(sentbytes==-1)
+		{
+			printf("!!");
+			close(sockfd);
+		}
+
+		sentbytes=send(sockfd,arr,n*sizeof(int),0);
+		if(sentbytes==-1)
+		{
+			printf("!!");
+			close(sockfd);
+		}
+		while(flag == 1){
+			int choice;
+			printf("\n\nEnter your choice:\n");
+			printf("1. Search for a number\n");
+			printf("2. Sort numbers\n");
+			printf("3. New Array\n");
+			printf("4. Exit program\n");
+			scanf("%d", &choice);
+
+			sentbytes=send(sockfd,&choice,sizeof(choice),0);
+			if(sentbytes==-1)
+			{
+				printf("!!");
+				close(sockfd);
+			}
+
+			if(choice == 1){
+				int num;
+				int res;
+				printf("\nWhich number do you want to search for ? : ");
+				scanf("%d",&num);
+
+				sentbytes=send(sockfd,&num,sizeof(num),0);
+				if(sentbytes==-1)
+				{
+					printf("!!");
+					close(sockfd);
+				}
+
+				recedbytes=recv(sockfd,&res,sizeof(res),0);
+				if(res != -1){
+					printf ("Message recieved : Number found at index %d !!\n", res);
+				}else{
+					printf ("Message recieved : Number not found \n");
+				}
+			}else if(choice == 2){
+				int flag;
+				printf("\nEnter 1 for Ascending order / 2 for Descending order.\n");
+				scanf("%d", &flag);
+
+				sentbytes=send(sockfd,&flag,sizeof(flag),0);
+				if(sentbytes==-1)
+				{
+					printf("!!");
+					close(sockfd);
+				}
+
+				recedbytes=recv(sockfd,arr,n * sizeof(int),0);
+				printf("Message recieved : Sorted array -> ");
+				for(int i  = 0 ; i < n ; i++){
+					printf("%d\t",arr[i]);
+				}
+				printf("\n");
+
+			}else if(choice == 3){
+				printf("\nGOING BACK TO STARTING OF THE PROGRAM!!\n");
+				flag = 0;
+			}else{
+				if(choice == 4) printf("\nEXITTING !!\n");
+				else printf("\ninvalid choice!! exitting !!\n");
+				close(sockfd);
+				return;
+			}
+		}
+		
+	}
+	close(sockfd);
+}
